c1118686760b06d9eadbd46d6fa64377bde4d194
Dominic Jones <haakym@gmail.com>
2016-11-15 20:24:01 +0000

    set GhostScript executable manually or determine based on OS/arch

--- src/Converter/GhostscriptConverterCommand.php
+++ src/Converter/GhostscriptConverterCommand.php
@@ -22,15 +22,72 @@ class GhostscriptConverterCommand
     /**
      * @var Filesystem
      */
-    protected $baseCommand = 'gs -sDEVICE=pdfwrite -dCompatibilityLevel=%s -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=%s %s';
+    protected $baseCommand = '%s -sDEVICE=pdfwrite -dCompatibilityLevel=%s -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=%s %s';
+
+    /**
+     * @var string
+     */
+    protected $executable;
 
     public function __construct()
     {
     }
 
+    public function setExecutable($executable)
+    {
+        $this->executable = $executable;
+    }
+
+    private function getExecutable()
+    {
+        return $this->executable ?: $this->getExecutableByOS();
+    }
+
+    private function getExecutableByOS()
+    {
+        $os = PHP_OS;
+
+        $executablesByOS = [
+            'WINNT' => [
+                '32' => 'gswin32c',
+                '64' => 'gswin64c'
+            ],
+            'LINUX' => [
+                'i686' => 'gs',
+                'x86_64' => 'gs'
+            ],
+        ];
+
+        if ($os === 'WINNT') {
+            $out = [];
+            exec("wmic cpu get DataWidth", $out);
+            $bits = strstr(implode("", $out), "64") ? 64 : 32;
+            $architecture = $bits;
+        } else {
+            $architecture = shell_exec('arch');
+        }
+
+        $executable = $executablesByOS[$os][$architecture];
+        
+        if (!$this->checkExecutableExists($executable)) {
+            throw new Exception("Cannot determine GhostScript exe");
+        }
+
+        return $executable;        
+    }
+
+    private function checkExecutableExists($executable)
+    {
+        if (!shell_exec('which ' . $executable)) {
+            return false;
+        }
+
+        return true;
+    }
+
     public function run($originalFile, $newFile, $newVersion)
     {
-        $command = sprintf($this->baseCommand, $newVersion, $newFile, $originalFile);
+        $command = sprintf($this->baseCommand, $this->getExecutable(), $newVersion, $newFile, $originalFile);
 
         $process = new Process($command);
         $process->run();
